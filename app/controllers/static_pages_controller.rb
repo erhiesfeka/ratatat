ee4a85203ea20aa78ff31f2657b8948745fc7468
erhiesfeka <erhiesfeka@yahoo.com>
2014-08-07 14:31:37 -0400

    first commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,12 @@
+class StaticPagesController < ApplicationController
+  def signin
+  end
+
+  def about
+  end
+
+  def home
+  end
+  def contact
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ratatat::Application.config.secret_key_base = '84ee201a4e6f99b6ae1240f3f6a737589cf7aea99f52a237d1010dad33741d91d21fac3e71c70cec834c4839ac786bf1dc69dfc923d798441059e45acffee87c'
