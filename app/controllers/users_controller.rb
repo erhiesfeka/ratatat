0dd0a8518fd7c72bbc120fa216e20c4ce7d9ff2e
erhiesfeka <erhiesfeka@yahoo.com>
2014-09-01 22:10:08 -0500

    Update user controller to work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,29 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update]
   before_action :correct_user,   only: [:edit, :update]
+ 
+  
   def show
     @user = User.find(params[:id])
   end
+
   def new
   	@user = User.new
   end
-     def create
+
+  def create
     @user = User.new(user_params)
     if @user.save
-       sign_in @user
+      sign_in @user
       flash[:success] = "Welcome to Ratatat!"
       redirect_to @user
     else
       render 'new'
     end
+  end
+
+  def edit
+    #@user = User.find(params[:id])
   end
 
   def update
@@ -25,6 +33,7 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       render 'edit'
+    end
   end
 
   private
@@ -33,13 +42,16 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+
     # Before filters
 
     def signed_in_user
       redirect_to signin_url, notice: "Please sign in." unless signed_in?
     end
-    def edit
-    @user = User.find(params[:id])
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
     
 end
