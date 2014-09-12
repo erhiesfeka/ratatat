5aa9592cfef9bd4af89371e9789a2a167c612f47
erhiesfeka <erhiesfeka@yahoo.com>
2014-09-12 18:19:39 -0400

    Include School

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Ratatat::Application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/school', to: 'static_pages#school', via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
